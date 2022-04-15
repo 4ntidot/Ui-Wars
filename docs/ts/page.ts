aa5f013f69e2b5f58fce7c5713426366f2104bee
happex <98713994+nao-l@users.noreply.github.com>
2022-04-15 13:37:29 +0200

    :stars: Create `page.ts`
    * For the page imports (topbar, footer...)
    * Useful with several pages

---
+++ docs/ts/page.ts
@@ -0,0 +1,17 @@
+var currentPath = "";
+
+function displayContent(path) {
+	// The path variable corresponds to the path from the current page to the root.
+	// We use /directory/index.html for every app or file manager that doesn’t understand /directory directly.
+	var currentPath: string = path;
+	const topbarContent = `
+	<a href="${path}/index.html" class="navLink"><img class="navIcon" src="${path}/favicon.png"></a>
+	<a href="${path}/about.html" class="navLink">About</a>
+	<a href="${path}/participate.html" class="navLink">Participate</a>
+	<a href="${path}/ideas.html" class="navLink">Ideas</a>
+	<a href="https://scratch.mit.edu/studios/31417547" class="navLink">Studio<a>
+	<a href="${path}/feedback" class="navButton">Feedback</a>
+	<a onClick="changeTheme()" class="navButton"><img class="navIcon" src="${path}/assets/theme.svg"></a>
+	`
+	document.getElementsByClassName("topbar")[0].innerHTML = topbarContent;
+}
